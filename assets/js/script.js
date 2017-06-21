2f106bb3b38ce18e75f248f214f41bcebdaa8907
Jonathan Costa <jonjcosta@gmail.com>
2017-06-21 16:18:26 -0300

    Basic operations
    - Added basic math operations
    - Added responses for "clear" button
    - Added response for "equal" button when no operation or second value is given

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,73 @@
-$(function() {
+$(function () {
 
-    var calcVal = 0;
+    reset();
+    // calculate();
 
-    setCalcVal();
 
-    $(".calc__btn").click(function() {
+    $(".calc__btn").click(function () {
         switch ($(this).val()) {
+            case "CL":
+                reset();
+                break;
+            case "=":
+                calculate();
+                break;
             case "+":
             case "-":
             case "x":
             case "%":
-            case "=":
-            case "CL":
-                setCalcVal(0);
-                break;
             default:
                 setCalcVal($(this).val());
 
         }
     });
 
-    function setCalcVal(newVal = 0) {
-        calcVal = newVal;
-        $(".calc__input").val(newVal);
+    function reset() {
+        $(".calc__input").val(0);
+    }
+
+    function setCalcVal(newVal) {
+        let curVal = $(".calc__input").val();
+        console.log(curVal);
+        if (/\d/g.test(newVal)) {
+            $(".calc__input").val(curVal == 0 ? +newVal : curVal + "" + newVal);
+        } else {
+            // It's a symbol, so we need to calculate OR add it to the query
+            if (/\D/g.test(curVal)) {
+                calculate();
+                curVal = $(".calc__input").val();
+            }
+            $(".calc__input").val(curVal + "" + newVal);
+        }
+    }
+
+    function calculate() {
+        let query = $(".calc__input").val();
+        let numbers = query.split(/[+|\-|*|%]/g);
+        if (!(numbers[1])) {
+            $(".calc__input").val(query.match(/\d/g).join(""));
+            return false;
+        }
+        let operation = query.match(/[+|\-|*|%]/g)[0];
+        numbers.forEach(function (item, index) {
+            numbers[index] = parseFloat(item);
+        });
+        let result = 0;
+        switch (operation) {
+            case '+':
+                result = numbers[0] + numbers[1];
+                break;
+            case '-':
+                result = numbers[0] - numbers[1];
+                break;
+            case '*':
+                result = numbers[0] * numbers[1];
+                break;
+            case '/':
+                result = numbers[1] != 0 ? numbers[0] / numbers[1] : 0;
+                break;
+        }
+        $(".calc__input").val(result);
     }
 
 });
