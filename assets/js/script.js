e460e4f9afc5abb82b82c0318a41c762b80f618c
Jonathan Costa <jonjcosta@gmail.com>
2017-06-22 17:31:37 -0300

    Start of query functions
    - Changed the number section from input to div.
    - Added a subtitle to print the operation query.
    - Added command to block click on 0 when preparing for division.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 $(function () {
 
     reset();
-    // calculate();
 
-
+    // Listener to the buttons
     $(".calc__btn").click(function () {
         switch ($(this).val()) {
             case "CL":
@@ -12,40 +11,54 @@ $(function () {
             case "=":
                 calculate();
                 break;
-            case "+":
-            case "-":
-            case "x":
-            case "%":
             default:
                 setCalcVal($(this).val());
-
         }
     });
 
+    // Resets the value back to 0
     function reset() {
-        $(".calc__input").val(0);
+        $(".calc__input").html(0);
+        $(".calc__query").html(0);
     }
 
+    // 
     function setCalcVal(newVal) {
-        let curVal = $(".calc__input").val();
+        let curVal = $(".calc__input").html();
+        let curQuery = $(".calc__query").html();
         console.log(curVal);
+        
         if (/\d/g.test(newVal)) {
-            $(".calc__input").val(curVal == 0 ? +newVal : curVal + "" + newVal);
+            // If the current operation is division, don't allow 0 to be pressed
+            if (/\%$/g.test(curVal) && newVal == "0") return false;
+            let val = curVal == 0 ? +newVal : curVal + "" + newVal;
+            $(".calc__input").html(val);
+            $(".calc__query").html(val);
         } else {
             // It's a symbol, so we need to calculate OR add it to the query
             if (/\D/g.test(curVal)) {
+                let numbers = curVal.split(/[+|\-|*|%]/g);
+                // Replaces the operation if there's already an active operation
+                if (!(numbers[1])) {
+                    $(".calc__input").html(curVal.replace(/[+|\-|*|%]/g, newVal));
+                    return false;
+                }
+                // If there's one query in order, calculate it and add the following symbol
                 calculate();
-                curVal = $(".calc__input").val();
+                curVal = $(".calc__input").html();
             }
-            $(".calc__input").val(curVal + "" + newVal);
+            $(".calc__input").html(curVal + "" + newVal);
         }
+        curVal = $(".calc__input").html();
+        $(".calc__query").html(curVal);
     }
 
+    // Reads the current query and shows the result
     function calculate() {
-        let query = $(".calc__input").val();
+        let query = $(".calc__input").html();
         let numbers = query.split(/[+|\-|*|%]/g);
         if (!(numbers[1])) {
-            $(".calc__input").val(query.match(/\d/g).join(""));
+            $(".calc__input").html(query.match(/[^+|\-|*|%]/g).join(""));
             return false;
         }
         let operation = query.match(/[+|\-|*|%]/g)[0];
@@ -63,11 +76,13 @@ $(function () {
             case '*':
                 result = numbers[0] * numbers[1];
                 break;
-            case '/':
+            case '%':
                 result = numbers[1] != 0 ? numbers[0] / numbers[1] : 0;
                 break;
         }
-        $(".calc__input").val(result);
+        $(".calc__input").html(result);
+        let curQuery = $(".calc__query").html();
+        $(".calc__query").html(curQuery+"="+result);
     }
 
 });
