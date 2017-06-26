5401ad8ecd485147e9cc34a3e5dea7f3a6a5c25c
JonCosta <jonjcosta@gmail.com>
2017-06-26 11:31:36 -0300

    Big.js added and multiple fixes
    - Included big.js library and changed calculate function  to use it
    - Reworked functions comments for more clarity
    - Removed useless lines
    - When the last input in a query is another operators, it won't be read anymore

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,31 @@ $(function () {
                 reset();
                 break;
             case "=":
-                calculate($(".calc__query").html());
+                if (!/=/g.test($(".calc__query").html())) {
+                    //We get an array with the grouped numbers and the operators in the query's order
+                    let entries = groupQueryNumbers($(".calc__query").html());
+                    calculate(entries);
+                }
                 break;
             default:
                 setCalcVal($(this).val());
         }
     });
 
-    // Resets the value back to 0
+    /**
+     * Resets the query and main values back to "0"
+     */
     function reset() {
         $(".calc__input").html(0);
         $(".calc__query").html(0);
     }
 
-    // 
+    /**
+     * Adds a new value (number or operator) to the current query
+     * @param {String} newVal Number or operator (1, 2, 3... or +, -, * ...)
+     */
     function setCalcVal(newVal) {
         let curVal = /=/g.test($(".calc__query").html()) ? '' : $(".calc__input").html();
-
         if (/\d/g.test(newVal)) {
             // Empties the value and the query if there's an "=" sign in the query
             let curQuery = /=/g.test($(".calc__query").html()) ? '' : $(".calc__query").html();
@@ -35,74 +43,68 @@ $(function () {
             $(".calc__input").html(/\D/g.test(curVal) ? newVal : val);
             $(".calc__query").html(curQuery == 0 ? +newVal : curQuery + "" + newVal);
         } else {
+            // If the query has a result, start a new query from the result
             let curQuery = /=/g.test($(".calc__query").html()) ? $(".calc__query").html().split("=")[1] : $(".calc__query").html();
-            // It's a symbol, so we need to calculate OR add it to the query
-            if (/[+|\-|x|\/]/g.test(curQuery)) {
-                let numbers = curQuery.split(/[+|\-|x|\/]/g);
-                // Replaces the operation if there's already an active operation
-                if (!(numbers[1])) {
-                    $(".calc__input").html(newVal);
-                    $(".calc__query").html(curQuery.replace(/[+|\-|x|\/]/g, newVal));
-                    return false;
-                }
-                // If there's one query in order, calculate it and add the following symbol
-                curQuery = $(".calc__query").html();
-                calculate(curQuery);
-            }
             $(".calc__input").html(newVal);
             $(".calc__query").html(curQuery + "" + newVal);
         }
     }
 
-    // Reads the current query and shows the result
-    function calculate(query) {
-        //We get an array with the grouped numbers and the operators in the query's order
-        let entries = groupQueryNumbers(query);
+    /**
+     * Reads a query,
+     * @param {String} query The math operation to be read
+     */
+    function calculate(entries) {
         // Will run through the entries array, working with each removed (shifted) value
         while (entries.length > 0) {
             // Obtains the first number value and declares the result container
             if (result === undefined) {
-                var result = parseFloat(entries.shift());
+                // var result = parseFloat(entries.shift());
+                var result = new Big(entries.shift());
             }
             let operation = entries.shift();
-            // Execute a different procedure based on the number/operator
+            // Execute a different procedure based on the number/operator]clg
+            console.log(operation);
             switch (operation) {
                 // Sum
                 case '+':
-                    result += parseFloat(entries.shift());
+                    result = result.plus(entries.shift());
                     break;
                 // Subtract
                 case '-':
-                    result -= parseFloat(entries.shift());
+                    result = result.minus(entries.shift());
                     break;
                 // Multiply
                 case 'x':
-                    result *= parseFloat(entries.shift());
+                    result = result.times(entries.shift());
                     break;
                 // Divide
                 case '/':
-                    result /= parseFloat(entries.shift());
+                    result = result.div(entries.shift());
                     break;
             }
-            console.log(Big(3.12).times(9));
 
         }
-
-        $(".calc__input").html(result);
+        // Prints the result in the main value
+        $(".calc__input").html(result.toString());
+        // Prints the result along with the full query
         let curQuery = $(".calc__query").html();
-        $(".calc__query").html(curQuery + "=" + result);
+        $(".calc__query").html(curQuery + "=" + result.toString());
     }
 
     /**
      * Reads the math query and return an array with numbers grouped and operators separated
-     * @param {String} query The math opreation to be read
+     * @param {String} query The math operation to be read
      */
     function groupQueryNumbers(query) {
         let arr = [];
+        // Runs through each element of the query
         query.split("").forEach(function (element, index) {
+            // If it's an operator, simply push it into arr
             if (/[+|\-|x|\/]/g.test(element)) {
                 arr.push(element);
             } else {
+                // Verifies for numbers with multiple digits and in floating point
                 if (/\d+\.?\d*/g.test(arr[arr.length - 1])) {
                     arr[arr.length - 1] += element;
                 } else {
@@ -110,7 +112,6 @@ $(function () {
                 }
             }
         });
-        console.log(parseFloat("3.12"));
         return arr;
     }
 
